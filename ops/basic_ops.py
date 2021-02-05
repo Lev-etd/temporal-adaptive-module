ae090164d09a8e664e8c8a08b091d0bf3583050d
Lev-etd <levevt2012@gmail.com>
2021-02-06 01:10:36 +0300

    Trying to fix deprecated nn.autograd.Function

--- ops/basic_ops.py
+++ ops/basic_ops.py
@@ -4,6 +4,7 @@ import torch
 class Identity(torch.nn.Module):
     @staticmethod
     def forward(ctx, input):
+        ctx.save_for_backward(input)
         return input
 
 
@@ -11,10 +12,11 @@ class SegmentConsensus(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input_tensor):
         ctx.consensus_type = consensus_type
-        ctx.dim = 1
+        ctx.dim = dim
         ctx.shape = input_tensor.size()
+        ctx.save_for_backward(consensus_type, dim, shape)
         if ctx.consensus_type == 'avg':
-            output = input_tensor.mean(dim=self.dim, keepdim=True)
+            output = input_tensor.mean(dim=ctx.dim, keepdim=True)
         elif ctx.consensus_type == 'identity':
             output = input_tensor
         else:
@@ -26,7 +28,7 @@ class SegmentConsensus(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output):
         if ctx.consensus_type == 'avg':
-            grad_in = grad_output.expand(self.shape) / float(ctx.shape[ctx.dim])
+            grad_in = grad_output.expand(ctx.shape) / float(ctx.shape[ctx.dim])
         elif ctx.consensus_type == 'identity':
             grad_in = grad_output
         else:
@@ -37,11 +39,10 @@ class SegmentConsensus(torch.autograd.Function):
 
 class ConsensusModule(torch.nn.Module):
 
-    def __init__(self, consensus_type, dim=1):
-        super(ConsensusModule, self).__init__()
-        self.consensus_type = consensus_type if consensus_type != 'rnn' else 'identity'
-        self.dim = dim
     
     @staticmethod
     def forward(ctx, input):
-        return SegmentConsensus.apply(input,(self.consensus_type, self.dim))
+        ctx.consensus_type = consensus_type if consensus_type != 'rnn' else 'identity'
+        ctx.dim = dim
+        ctx.save_for_backward(input, consensus_type, dim)
+        return SegmentConsensus.apply(input,(ctx.consensus_type, ctx.dim))

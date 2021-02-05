b6c373f9b5dffa5ec1f529340764e5b2459beea5
Lev-etd <levevt2012@gmail.com>
2021-02-06 00:13:41 +0300

    Trying to fix deprecated nn.autograd.Function

--- ops/basic_ops.py
+++ ops/basic_ops.py
@@ -7,19 +7,15 @@ class Identity(torch.nn.Module):
         return input
 
 
-class SegmentConsensus(torch.autograd.Function):
-
-    def __init__(self, consensus_type, dim=1):
-        self.consensus_type = consensus_type
-        self.dim = dim
-        self.shape = None
-    
+class SegmentConsensus(torch.autograd.Function):  
     @staticmethod
-    def forward(self, input_tensor):
-        self.shape = input_tensor.size()
-        if self.consensus_type == 'avg':
+    def forward(ctx, input_tensor):
+        ctx.consensus_type = consensus_type
+        ctx.dim = 1
+        ctx.shape = input_tensor.size()
+        if ctx.consensus_type == 'avg':
             output = input_tensor.mean(dim=self.dim, keepdim=True)
-        elif self.consensus_type == 'identity':
+        elif ctx.consensus_type == 'identity':
             output = input_tensor
         else:
             output = None
@@ -28,10 +24,10 @@ class SegmentConsensus(torch.autograd.Function):
 
     
     @staticmethod
-    def backward(self, grad_output):
-        if self.consensus_type == 'avg':
-            grad_in = grad_output.expand(self.shape) / float(self.shape[self.dim])
-        elif self.consensus_type == 'identity':
+    def backward(ctx, grad_output):
+        if ctx.consensus_type == 'avg':
+            grad_in = grad_output.expand(self.shape) / float(ctx.shape[ctx.dim])
+        elif ctx.consensus_type == 'identity':
             grad_in = grad_output
         else:
             grad_in = None
@@ -41,11 +37,10 @@ class SegmentConsensus(torch.autograd.Function):
 
 class ConsensusModule(torch.nn.Module):
 
-    def __init__(self, consensus_type, dim=1):
-        super(ConsensusModule, self).__init__()
-        self.consensus_type = consensus_type if consensus_type != 'rnn' else 'identity'
-        self.dim = dim
+
     
     @staticmethod
     def forward(self, input):
-        return SegmentConsensus(self.consensus_type, self.dim)(input)
+        ctx.consensus_type = consensus_type if consensus_type != 'rnn' else 'identity'
+        ctx.dim = dim
+        return SegmentConsensus.apply(input,(ctx.consensus_type, ctx.dim))
